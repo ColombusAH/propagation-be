f3e705ce8981e39524232882c96515163b18a5a5
Avner Hattab <avner.h@nanox.vision>
2025-04-11 16:57:12 +0300

    xc

--- be/app/api/v1/endpoints/auth.py
+++ be/app/api/v1/endpoints/auth.py
@@ -5,3 +5,14 @@ router = APIRouter()
 @router.get("/")
 async def auth_root():
     return {"message": "Auth endpoint"} 
+
+
+@router.get('/me')
+async def get_me():
+    # return a mock rick user info 
+    return {
+        "id": 1,
+        "name": "Rick",
+        "email": "rick@example.com",
+        "role": "admin"
+    }
--- fe/src/routes/__root.tsx
+++ fe/src/routes/__root.tsx
@@ -1,11 +1,17 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <MantineProvider theme={theme}>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </MantineProvider>
   ),
 })
