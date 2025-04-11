92f9ed61a051b5b3d8ff911cb22eb22d071b0cae
Avner Hattab <avner.h@nanox.vision>
2025-04-11 17:44:22 +0300

    fg

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -5,7 +5,7 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/notifications/styles.css';
 import '@mantine/dropzone/styles.css';
-// Import the generated route tree
+// Import the generated route tree.
 import { routeTree } from './routeTree.gen'
 
 import './styles.css'
